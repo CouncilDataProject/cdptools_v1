0e3ad222d7c2cd49bd9d4dd9f37f776b6e6d3fd8
JacksonMaxfield <jacksonb@alleninstitute.org>
2018-04-16 22:42:02 -0700

    sea_utils for better understanding

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 from cdp.io.pipelines import BodyPipe as bp
-from cdp.utils.seattle import *
+from cdp.utils import seattle as sea_utils
 from pprint import pprint
 
 city = "seattle"
-seattle_bodies = bp(city, body_name_shortener)
+seattle_bodies = bp(city, sea_utils.body_name_shortener)
 
 pprint(seattle_bodies.short_names())

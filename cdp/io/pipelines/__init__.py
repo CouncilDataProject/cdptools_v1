187f0b6413f0b2b98515ce74b969dd9847f5d768
JacksonMaxfield <jacksonb@alleninstitute.org>
2018-04-19 17:29:36 -0700

    init broke

--- cdp/io/pipelines/__init__.py
+++ cdp/io/pipelines/__init__.py
@@ -1,3 +1,3 @@
-from .bodypipe import BodyPipe
-from .videopipe import VideoPipe
-from .legistarpipe import LegistarPipe
+from .BodyPipe import BodyPipe
+from .VideoPipe import VideoPipe
+from .LegistarPipe import LegistarPipe
